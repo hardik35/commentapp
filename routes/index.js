9177000102b39d42c47a7e8f96f61705e97ba564
hardik35 <hardikarora355@gmail.com>
2021-07-22 15:08:17 +0530

    a

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const todosController = require('../controllers').todos;
+const todosItemsController = require('../controllers').todoItems;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Todos API!',
+  }));
+
+  app.post('/api/todos', todosController.create);
+  app.get('/api/todos', todosController.list);
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  app.post('/api/todos/:todoId/items', todosItemsController.create);
+};
