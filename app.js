0848327cf2147f8a7180658868265acb80483aa9
hardik35 <hardikarora355@gmail.com>
2021-07-26 00:05:35 +0530

    rest

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require our routes into the application.
 require('./routes')(app);
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+  message: 'Welcome to the beg of nothingness.',
 }));
 
 module.exports = app;
